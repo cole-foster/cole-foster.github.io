651e396259cd99c9f78d338a70da5987985feb2b
Cole Foster <colefoster@Coles-Laptop.local>
2023-04-08 07:56:18 -0400

    changes

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-// add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+// // add class navbarDark on navbar scroll
+// const header = document.querySelector('.navbar');
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navbarDark');
+//     }
+//     else {
+//         header.classList.remove('navbarDark');
+//     }
+// }
